fc00b859335bae1700bcea827c2ea0a99e24d168
arnonsang <arnon.sangn@mail.kmutt.ac.th>
2023-08-02 17:30:22 +0700

    080223 1730 | Add API Info

--- packages/server/routes/index.js
+++ packages/server/routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var platform = require('platform');
+var axios = require('axios');
+const { log } = require('../logger');
+
 
 /**
  * @swagger
@@ -8,7 +12,35 @@ var router = express.Router();
  *     summary: Main page of API server and API documentation
  *     description: This is the main page of API server for GEP SWU Website. You can find all the API documentation here.
 */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  const ua = platform.parse(req.get('User-Agent'));
+  res.locals.ip = req.ip;
+  //not fetch ipinfo if localhost
+  res.locals.hostname = 'localhost';
+  res.locals.city = 'localhost';
+  res.locals.region = 'localhost';
+  res.locals.country = 'localhost';
+  res.locals.loc = 'localhost';
+  res.locals.org = 'localhost';
+  res.locals.postal = 'localhost';
+  res.locals.timezone = 'localhost';
+  res.locals.readme = 'localhost';
+
+  if (!req.ip === '::1' || !req.ip === '::ffff:'){
+    const ipInfo = await axios.get(`https://ipinfo.io/${req.ip}?token=4c71014842cc6a`).then(res => res.data).catch(err => `Unknown`);
+    log("Server", ipInfo);
+    res.locals.hostname = ipInfo.hostname;
+    res.locals.city = ipInfo.city;
+    res.locals.region = ipInfo.region;
+    res.locals.country = ipInfo.country;
+    res.locals.loc = ipInfo.loc;
+    res.locals.org = ipInfo.org;
+    res.locals.postal = ipInfo.postal;
+    res.locals.timezone = ipInfo.timezone;
+  }
+
+  res.locals.userAgent = req.get('User-Agent');
+  res.locals.ua = ua;
   res.render('index', { title: 'GEP-SWU Server' });
 });
 
