4f28ec98a3165e5af68075a1cc2867305333cd27
arnonsang <arnon.sangn@mail.kmutt.ac.th>
2023-08-06 18:27:56 +0700

    080623 1827 | Add Development env handlers

--- packages/client/src/lib/API/baseapi.js
+++ packages/client/src/lib/API/baseapi.js
@@ -1,3 +1,28 @@
-//export const API = 'http://localhost:5001/';
-export const API = 'https://gepswu-server.onrender.com/';
+// Note: This file is used to determine which API to use
+//       If the server is running, use the local API
+//       If the server is not running, use the remote API
+//       This is to prevent the client from crashing when the server is not running
+//       This is also to prevent the client from crashing when the server is running
+//       but the API is not working
+const axios = require("axios");
+
+const getAvailableAPI = async () => {
+  let api = "https://localhost:5001/";
+  const response = await axios
+    .get(`${api}health`)
+    .then((res) => {
+      if (res.status === 200) {
+        api = "https://localhost:5001/";
+      }
+      api = "https://gepswu-server.onrender.com/";
+      return api;
+    })
+    .catch((err) => {
+      api = "https://gepswu-server.onrender.com/";
+      return api;
+    });
+  return api;
+};
+
+export const API = getAvailableAPI();
 export default API;
