222fed686785dca9765b0a03ddf942aa006d5f79
TakatoraSama <weeraphan2545@gmail.com>
2023-08-20 21:26:52 +0700

    200823 2134 | add personnels page

---
+++ packages/client/src/components/PersonelProfile.svelte
@@ -0,0 +1,20 @@
+<script>
+    import lang from "../lib/lang";
+    import getImg from "../lib/getImg";
+    import { get } from "../lib/API/methods";
+
+    export let data;
+
+    let display = lang() || "th";
+</script>
+
+<div class="w-[414px] h-[175px] bg-amber-100 mt-4">
+    <img
+    src={getImg(data.img)}
+    alt={data.email + "_IMG"}
+    class="max-h-[175px]"
+    />
+    <p>
+        {data.name}
+    </p>
+</div>
---
+++ packages/client/src/pages/Personel/Personel.svelte
@@ -0,0 +1,31 @@
+<script>
+    import PersonelProfile from "../../components/PersonelProfile.svelte";
+    import { get } from "../../lib/API/methods";
+
+    let personnelsData = get("personnels");
+</script>
+
+<svelte:head>
+    <title>Personel</title>
+    <meta property="og:title" content="SWU" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="" />
+</svelte:head>
+
+<div class="min-h-full">
+    <h2 class="text-xl md:text-3xl font-bold drop-shadow-md text-[#00A2E0] ml-12" >
+        บุคลากร
+    </h2>
+    <p class="text-base ml-20">
+        อาจารย์ภาควิชาการแนะแนวและจิตวิทยาการศึกษา
+    </p>
+
+    {#await personnelsData}
+        <div />
+    {:then personnelData} 
+        {#each personnelData.data as data}
+            <PersonelProfile {data}/>
+        {/each}
+    {/await}
+</div>
