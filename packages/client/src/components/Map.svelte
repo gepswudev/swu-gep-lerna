98c55dec06300711bbfd7d2e1de4d041fc985b76
arnonsang <arnon.sangn@mail.kmutt.ac.th>
2023-08-22 18:17:45 +0700

    082223 1817 | Optimize map marker

--- packages/client/src/components/Map.svelte
+++ packages/client/src/components/Map.svelte
@@ -1,21 +1,24 @@
 <script>
   import { onMount, onDestroy } from "svelte";
+  import Logo from "../assets/Srinakharinwirot_University_Logo.png";
+  import { navigate } from "svelte-routing";
 
   export let marker = {
     lat: 13.745518582850021,
     lng: 100.5649431162508,
     text: "ภาควิชาการแนะแนวและจิตวิทยาการศึกษา",
+    navigate:"#"
   };
 
   let mapElement;
   let map;
 
   onMount(async () => {
-    const leaflet = await import("leaflet");
+    const L = await import("leaflet");
 
-    map = leaflet.map(mapElement).setView([marker.lat, marker.lng], 17);
+    map = L.map(mapElement).setView([marker.lat, marker.lng], 17);
 
-    leaflet
+    L
       .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         maxZoom: 19,
         attribution:
@@ -23,8 +26,17 @@
       })
       .addTo(map);
 
-    leaflet
-      .marker([marker.lat, marker.lng])
+    L
+      .marker([marker.lat, marker.lng],{
+        icon: L.icon({
+          iconUrl: Logo,
+          iconSize: [30, 30],
+          iconAnchor: [15,30],
+          popupAnchor: [0, -25],
+        }),
+        title: marker.text,
+        autoPanOnFocus: true,
+      })
       .addTo(map)
       .bindPopup(marker.text)
       .openPopup();
@@ -44,7 +56,9 @@
 </script>
 
 <main>
-  <div bind:this={mapElement} on:click={ (e) => console.log(e)}/>
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div bind:this={mapElement} on:click={ (e) => navigate(marker.navigate)} title="Click to navigate"/>
 </main>
 
 <style>
--- packages/client/src/pages/Contact/Map.svelte
+++ packages/client/src/pages/Contact/Map.svelte
@@ -6,13 +6,14 @@
     lat: 13.745338582850021,
     lng: 100.56419431162508,
     text: metadata.base.description,
+    navigate:"https://www.google.com/maps/dir//%E0%B8%84%E0%B8%93%E0%B8%B0%E0%B8%A8%E0%B8%B6%E0%B8%81%E0%B8%A9%E0%B8%B2%E0%B8%A8%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%A3%E0%B9%8C+%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%A7%E0%B8%B4%E0%B8%97%E0%B8%A2%E0%B8%B2%E0%B8%A5%E0%B8%B1%E0%B8%A2%E0%B8%A8%E0%B8%A3%E0%B8%B5%E0%B8%99%E0%B8%84%E0%B8%A3%E0%B8%B4%E0%B8%99%E0%B8%97%E0%B8%A3%E0%B8%A7%E0%B8%B4%E0%B9%82%E0%B8%A3%E0%B8%92+%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%A7%E0%B8%B4%E0%B8%97%E0%B8%A2%E0%B8%B2%E0%B8%A5%E0%B8%B1%E0%B8%A2%E0%B8%A8%E0%B8%A3%E0%B8%B5%E0%B8%99%E0%B8%84%E0%B8%A3%E0%B8%B4%E0%B8%99%E0%B8%97%E0%B8%A3%E0%B8%A7%E0%B8%B4%E0%B9%82%E0%B8%A3%E0%B8%92+114+%E0%B8%8B.+%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B8%B8%E0%B8%A1%E0%B8%A7%E0%B8%B4%E0%B8%97+23+%E0%B9%81%E0%B8%82%E0%B8%A7%E0%B8%87%E0%B8%84%E0%B8%A5%E0%B8%AD%E0%B8%87%E0%B9%80%E0%B8%95%E0%B8%A2%E0%B9%80%E0%B8%AB%E0%B8%99%E0%B8%B7%E0%B8%AD+%E0%B9%80%E0%B8%82%E0%B8%95%E0%B8%A7%E0%B8%B1%E0%B8%92%E0%B8%99%E0%B8%B2+%E0%B8%81%E0%B8%A3%E0%B8%B8%E0%B8%87%E0%B9%80%E0%B8%97%E0%B8%9E%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%99%E0%B8%84%E0%B8%A3+10110/@13.745205,100.5615699,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x30e29c3329e6aaef:0x4f1a7efa17dcd9df!2m2!1d100.5641448!2d13.7451998!3e0?entry=ttu"
   };
 </script>
 
 <div>
-  <div class="flex flex-col-reverse md:grid md:grid-cols-2 grid-flow-row-dense h-full pt-0 md:p-16 md:pb-56">
+  <div class="flex flex-col-reverse md:grid md:grid-cols-2 grid-flow-row-dense h-full pt-0 md:p-16 md:pb-32">
     <div class="hover:shadow-sm hover:shadow-primary">
-      <Map {marker} --map-height="25rem" />
+      <Map {marker} --map-height="30rem" />
     </div>
     <div class="mx-10 flex flex-col gap-4 justify-center py-12">
       <h3 class="text-2xl font-bold">ติดต่อเรา</h3>
@@ -25,7 +26,7 @@
       </p>
       <a
         class="btn btn-primary text-xl"
-        href={`https://www.google.com/maps/dir//%E0%B8%84%E0%B8%93%E0%B8%B0%E0%B8%A8%E0%B8%B6%E0%B8%81%E0%B8%A9%E0%B8%B2%E0%B8%A8%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%A3%E0%B9%8C+%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%A7%E0%B8%B4%E0%B8%97%E0%B8%A2%E0%B8%B2%E0%B8%A5%E0%B8%B1%E0%B8%A2%E0%B8%A8%E0%B8%A3%E0%B8%B5%E0%B8%99%E0%B8%84%E0%B8%A3%E0%B8%B4%E0%B8%99%E0%B8%97%E0%B8%A3%E0%B8%A7%E0%B8%B4%E0%B9%82%E0%B8%A3%E0%B8%92+%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%A7%E0%B8%B4%E0%B8%97%E0%B8%A2%E0%B8%B2%E0%B8%A5%E0%B8%B1%E0%B8%A2%E0%B8%A8%E0%B8%A3%E0%B8%B5%E0%B8%99%E0%B8%84%E0%B8%A3%E0%B8%B4%E0%B8%99%E0%B8%97%E0%B8%A3%E0%B8%A7%E0%B8%B4%E0%B9%82%E0%B8%A3%E0%B8%92+114+%E0%B8%8B.+%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B8%B8%E0%B8%A1%E0%B8%A7%E0%B8%B4%E0%B8%97+23+%E0%B9%81%E0%B8%82%E0%B8%A7%E0%B8%87%E0%B8%84%E0%B8%A5%E0%B8%AD%E0%B8%87%E0%B9%80%E0%B8%95%E0%B8%A2%E0%B9%80%E0%B8%AB%E0%B8%99%E0%B8%B7%E0%B8%AD+%E0%B9%80%E0%B8%82%E0%B8%95%E0%B8%A7%E0%B8%B1%E0%B8%92%E0%B8%99%E0%B8%B2+%E0%B8%81%E0%B8%A3%E0%B8%B8%E0%B8%87%E0%B9%80%E0%B8%97%E0%B8%9E%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%99%E0%B8%84%E0%B8%A3+10110/@13.745205,100.5615699,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x30e29c3329e6aaef:0x4f1a7efa17dcd9df!2m2!1d100.5641448!2d13.7451998!3e0?entry=ttu`}
+        href={marker.navigate}
         >นำทาง</a
       >
     </div>
