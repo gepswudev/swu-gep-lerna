6d418cd46c9a5da72e85c358d0b35923ca817197
TakatoraSama <weeraphan2545@gmail.com>
2023-08-26 15:02:17 +0700

    260823 1510 | decorate Counsult Step and add BG in Well Center page

--- packages/client/src/pages/WellCenter/WellCenter.svelte
+++ packages/client/src/pages/WellCenter/WellCenter.svelte
@@ -3,7 +3,12 @@
     import CounselorCard from "./CounselorCard.svelte";
     import Loading from "../../components/Loading.svelte"
     import Carousel from "svelte-carousel";
-    import {IconMessage, IconUsers, IconHeartbeat, IconWorld, IconSchool} from "@tabler/icons-svelte";
+    import {IconMessage, IconUsers, IconHeartbeat, IconWorld, IconSchool, IconHeartHandshake} from "@tabler/icons-svelte";
+    import EvaluateQr from "./evaluateQrcode1.png"
+    import RegisQr from "./regisQrcode1.png"
+    import FacebookIcon from "./facebook.svg"
+    import ObjectiveBg from "./WellCenterObjBg.png"
+    import Header from "./WellCenterHeader.png"
 
     let personnelsData = get("personnels");
 </script>
@@ -12,6 +17,8 @@
     {#await personnelsData}
         <Loading />
     {:then personnelData} 
+        <img src={Header} alt="" class="bg-cover">
+
         <!-- Mobile Carousel -->
         <div class="flex lg:hidden"> 
             <Carousel>
@@ -98,7 +105,7 @@
         </div>
 
         <!-- จุดประสงค์ -->
-        <div class="flex flex-col items-center"> 
+        <div class="flex flex-col items-center bg-cover bg-center w-full py-24" style="background-image: url({ObjectiveBg});"> 
             <p class="text-[36px] my-8"> จุดประสงค์ </p>
             <div class="grid grid-cols-2 lg:grid-cols-5 gap-12">
                 <div class="flex flex-col w-[200px] h-fit justify-center items-center">
@@ -154,37 +161,73 @@
         </div>
 
         <!-- ขั้นตอน และ เอกสารสำคัญ -->
-        <p class="text-[36px] text-neutral"> เริ่มรับคำปรึกษากับเรา </p>
-        <div class="flex flex-row text-neutral">
-            <div class="flex flex-col border-dashed border-2 w-[360px] pl-7 gap-6 py-7 rounded-xl">
-                <div class="flex items-center">
-                    <div class="flex items-center justify-center rounded-full w-[38px] h-[38px] bg-white border-black border-2 text-[14px]"> 1 </div>
-                    <p class="text-[16px] ml-6"> อ่านสิทธิของผู้มาปรึกษา  </p>
+        <div class="flex flex-col py-24 w-screen justify-center text-neutral bg-gradient-to-t from-[#00A2E0] to-white">
+            <div class="flex flex-row text-neutral justify-center gap-32">
+                <div class="flex flex-col justify-center">
+                    <div class="flex w-full justify-center items-center">
+                        <p class="text-[36px] text-neutral my-8"> เริ่มรับคำปรึกษากับเรา </p>
+                    </div>
+                    <div class="flex flex-col border-dashed border-2 w-[360px] pl-7 py-7 gap-6 justify-center rounded-xl border-[#9747FF]">
+                        <div class="flex items-center">
+                            <div class="flex items-center justify-center rounded-full w-[38px] h-[38px] bg-white border-black border-2 text-[14px]"> 1 </div>
+                            <p class="text-[16px] ml-6"> อ่านสิทธิของผู้มาปรึกษา  </p>
+                        </div>
+                        <div class="flex items-center">
+                            <div class="flex items-center justify-center rounded-full w-[38px] h-[38px] bg-white border-black border-2 text-[14px]"> 2 </div>
+                            <p class="text-[16px] ml-6"> ดูตารางวันเวลาที่สะดวกเข้ารับปรึกษา </p>
+                        </div>
+                        <div class="flex items-center">
+                            <div class="flex items-center justify-center rounded-full w-[38px] h-[38px] bg-white border-black border-2 text-[14px]"> 3 </div>
+                            <p class="text-[16px] ml-6"> Scan ลงทะเบียนเข้ารับปรึกษา </p>
+                        </div>
+                        <div class="flex items-center">
+                            <div class="flex items-center justify-center rounded-full w-[38px] h-[38px] bg-white border-black border-2 text-[14px]"> 4 </div>
+                            <p class="text-[16px] ml-6"> เข้ารับการปรึกษา </p>
+                        </div>
+                        <div class="flex items-center">
+                            <div class="flex items-center justify-center rounded-full w-[38px] h-[38px] bg-white border-black border-2 text-[14px]"> 5 </div>
+                            <p class="text-[16px] ml-6"> Scan แบบประเมินผลเข้ารับการปรึกษา </p>
+                        </div>
+                    </div>
                 </div>
-                <div class="flex items-center">
-                    <div class="flex items-center justify-center rounded-full w-[38px] h-[38px] bg-white border-black border-2 text-[14px]"> 2 </div>
-                    <p class="text-[16px] ml-6"> ดูตารางวันเวลาที่สะดวกเข้ารับปรึกษา </p>
+                <div class="mt-64">
+                    <div class="flex flex-row w-[595px] h-[120px] bg-info rounded-3xl justify-center items-center relative">
+                        <div class="flex flex-col text-neutral justify-center items-center text-[8px] font-bold absolute left-8">
+                            <IconHeartHandshake size=72 class="stroke-1"/>
+                            <p> ศูนย์ให้คำปรึกษา </p>
+                            <p> W.E.L.L. Center </p>
+                            <p> มศว </p>
+                        </div>
+                        <div class="flex flex-col text-center ">
+                            <p class="text-neutral text-[20px] font-bold"> เอกสารสำคัญ </p>
+                            <p class="text-neutral text-[28px] font-bold"> สิทธิของผู้มาปรึกษา </p>
+                            <p class="text-neutral text-[20px] font-bold"> โปรดอ่าน </p>
+                        </div>
+                    </div>
+                    <div class="flex flex-row justify-between items-center mx-8 mt-8">
+                        <div class="flex flex-col items-center">
+                            <img src={FacebookIcon} alt="" class="mb-4">
+                            <p> ศูนย์ให้คำปรึกษา </p>
+                            <p> W.E.L.L. Center มศว </p>
+                        </div>
+                        <div>
+                            <div class="flex bg-[#FF7171] w-[180px] h-[180px] justify-center items-center rounded-xl">
+                                <img src={RegisQr} alt="" class="w-[156px] h-[156px] ">
+                            </div>
+                            <div class="flex bg-[#FF7171] w-[180px] h-fit justify-center items-center rounded-full mt-4">
+                                <p class="text-[16px] text-base-100"> ลงทะเบียน Click </p>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="flex bg-[#FFBE71] w-[180px] h-[180px] justify-center items-center rounded-xl">
+                                <img src={EvaluateQr} alt="" class="w-[156px] h-[156px] ">
+                            </div>
+                            <div class="flex bg-[#FFBE71] w-[180px] h-fit justify-center items-center rounded-full mt-4">
+                                <p class="text-[16px] text-base-100"> ประเมิน Click </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="flex items-center">
-                    <div class="flex items-center justify-center rounded-full w-[38px] h-[38px] bg-white border-black border-2 text-[14px]"> 3 </div>
-                    <p class="text-[16px] ml-6"> Scan ลงทะเบียนเข้ารับปรึกษา </p>
-                </div>
-                <div class="flex items-center">
-                    <div class="flex items-center justify-center rounded-full w-[38px] h-[38px] bg-white border-black border-2 text-[14px]"> 4 </div>
-                    <p class="text-[16px] ml-6"> เข้ารับการปรึกษา </p>
-                </div>
-                <div class="flex items-center">
-                    <div class="flex items-center justify-center rounded-full w-[38px] h-[38px] bg-white border-black border-2 text-[14px]"> 5 </div>
-                    <p class="text-[16px] ml-6"> Scan แบบประเมินผลเข้ารับการปรึกษา </p>
-                </div>
-            </div>
-            <div>
-                <div class="w-[595px] h-[120px] bg-info rounded-3xl">
-                    <p class="text-neutral"> เอกสารสำคัญ </p>
-                    <p class="text-neutral"> สิทธิของผู้มาปรึกษา </p>
-                    <p class="text-neutral"> โปรดอ่าน </p>
-                </div>
-                <div></div>
             </div>
         </div>
     {/await}
