a9f273aee9092ccf8ddc6bb0347c0c59c166825f
arnonsang <arnon.sangn@mail.kmutt.ac.th>
2023-09-12 23:43:41 +0700

    091223 1143 | Add Markdown editor downloadable

--- packages/client/src/pages/Editor.svelte
+++ packages/client/src/pages/Editor.svelte
@@ -2,17 +2,29 @@
   import AdminProtect from "../components/admin/adminProtect.svelte";
   import {marked } from 'marked';
   let value = `# Hello World`;
+
+  const download = () => {
+    const element = document.createElement("a");
+    const file = new Blob([value], {type: 'text/plain'});
+    element.href = URL.createObjectURL(file);
+    element.download = "profile.md";
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+    URL.revokeObjectURL(element.href);
+  }
 </script>
 
 <!-- Test playground -->
-<AdminProtect>
+<!-- <AdminProtect> -->
+  <button class="btn btn-primary w-full m-4 " on:click={download}>Download</button>
   <div class="pt-12 grid grid-cols-2 gap-4 mx-2 min-h-screen">
     <div>
-      <textarea class="w-full bg-transparent border-2" bind:value={value}></textarea>
+      <textarea class="w-full h-full bg-transparent border-2" bind:value={value}></textarea>
     </div>
     <div class="prose max-w-none">
       {@html marked(value)}
     </div>
   </div>
-</AdminProtect>
+  <button class="btn btn-primary w-full m-4 " on:click={download}>Download</button>
+<!-- </AdminProtect> -->
 <!-- Test playground -->
